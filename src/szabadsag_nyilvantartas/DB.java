183a1d0074b80a919134fbf2a64d8747f35ac654
Polgár Béla <S@Lenovo-PC>
2019-03-03 19:31:23 +0100

    Szabi kezdet előbbi e mint a vége

--- src/szabadsag_nyilvantartas/DB.java
+++ src/szabadsag_nyilvantartas/DB.java
@@ -16,15 +16,16 @@ import javafx.collections.ObservableList;
 
 /**
  * Adatbázis kezelő osztály
- * @author  Polgár Béla
+ *
+ * @author Polgár Béla
  */
 public class DB {
-    
+
     final String dbUrl = "jdbc:mysql://localhost:3306/nyilvantarto2019?useUnicode=true&characterEncoding=UTF-8";
     final String user = "root";
     final String pass = "";
-    
-    public void dolgozo_lista (ObservableList<Dolgozo> tabla, ObservableList<String> lista) { //dolgozók listázása
+
+    public void dolgozo_lista(ObservableList<Dolgozo> tabla, ObservableList<String> lista) { //dolgozók listázása
         String s = "SELECT * FROM dolgozok;";
         try (Connection kapcs = DriverManager.getConnection(dbUrl, user, pass);
                 PreparedStatement ekp = kapcs.prepareStatement(s)) {
@@ -33,21 +34,21 @@ public class DB {
             lista.clear();
             while (eredmeny.next()) {
                 tabla.add(new Dolgozo(
-                    eredmeny.getInt(1),
-                    eredmeny.getString(2),
-                    eredmeny.getString(3),
-                    eredmeny.getInt(4),
-                    eredmeny.getInt(5),
-                    eredmeny.getInt(6),
-                    eredmeny.getInt(7)));
+                        eredmeny.getInt(1),
+                        eredmeny.getString(2),
+                        eredmeny.getString(3),
+                        eredmeny.getInt(4),
+                        eredmeny.getInt(5),
+                        eredmeny.getInt(6),
+                        eredmeny.getInt(7)));
                 lista.add(eredmeny.getString(2));
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
     }
-    
-    public void szabadsag_lista (ObservableList<Szabadsag> tabla) { //összes sabadság listázása
+
+    public void szabadsag_lista(ObservableList<Szabadsag> tabla) { //összes sabadság listázása
         String s = "SELECT szabadsagok.id,dolgozok.nev,szabadsag_kezdete,szabadsag_vege,szabadsag_hossza FROM szabadsagok JOIN dolgozok ON szabadsagok.dolgozoid=dolgozok.id ORDER BY szabadsagok.id;";
         try (Connection kapcs = DriverManager.getConnection(dbUrl, user, pass);
                 PreparedStatement ekp = kapcs.prepareStatement(s)) {
@@ -55,29 +56,29 @@ public class DB {
             tabla.clear();
             while (eredmeny.next()) {
                 tabla.add(new Szabadsag(
-                    eredmeny.getInt(1),
-                    eredmeny.getString(2),
-                    eredmeny.getString(3),
-                    eredmeny.getString(4),
-                    eredmeny.getInt(5)));
+                        eredmeny.getInt(1),
+                        eredmeny.getString(2),
+                        eredmeny.getString(3),
+                        eredmeny.getString(4),
+                        eredmeny.getInt(5)));
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
     }
-    
-        public void dolgozo_torles (int id){ 
+
+    public void dolgozo_torles(int id) {
         //dolgozó törlése
         String s = "DELETE FROM dolgozok WHERE id = ?;";
         try (Connection kapcs = DriverManager.getConnection(dbUrl, user, pass);
-            PreparedStatement ekp = kapcs.prepareStatement(s)) {
+                PreparedStatement ekp = kapcs.prepareStatement(s)) {
             ekp.setInt(1, id);
             ekp.executeUpdate();
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
     }
-        
+
     public void ujdolgozo(String nev, String szuletesi_datum, int gyerekek_szama) {
         //új dolgozó adatainak felvitele az adatok táblába
         String s = "INSERT INTO dolgozok (nev,szuletesi_datum,gyerekek_szama,eves_szabadsag,maradek_szabadsag) VALUES(?,?,?,?,?);";
@@ -90,12 +91,12 @@ public class DB {
             ekp.setInt(5, setEvesSzabi(szuletesi_datum, gyerekek_szama));
 
             ekp.executeUpdate();
-            
+
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
     }
-    
+
     private int setEvesSzabi(String szulido, int gyerekdb) {
         //éves alapszabadság meghatározása
         int eveszabadsag = 20;
@@ -134,8 +135,8 @@ public class DB {
         }
         return eveszabadsag;
     }
-    
-    public void dolgozo_modositas(int id,String nev, String szuletesiDatum, int gyerekDB){
+
+    public void dolgozo_modositas(int id, String nev, String szuletesiDatum, int gyerekDB) {
         //dolgozói adatok modosítása
         String s = "UPDATE dolgozok SET nev =?,szuletesi_datum=?,gyerekek_szama=?,eves_szabadsag=? WHERE id =?;";
         try (Connection kapcs = DriverManager.getConnection(dbUrl, user, pass);
@@ -150,8 +151,8 @@ public class DB {
             System.out.println(ex.getMessage());
         }
     }
-    
-    public void maradek_szabadsag_modositas (String nev, int valtozas){
+
+    public void maradek_szabadsag_modositas(String nev, int valtozas) {
         String s = "UPDATE dolgozok SET maradek_szabadsag = maradek_szabadsag + ? WHERE nev = ?;";
         try (Connection kapcs = DriverManager.getConnection(dbUrl, user, pass);
                 PreparedStatement ekp = kapcs.prepareStatement(s)) {
@@ -162,53 +163,52 @@ public class DB {
             System.out.println(ex.getMessage());
         }
     }
-    
-        public void uj_szabadsag(String nev, String szabistart, String szabivege) {
-        
-            //maradekszabi csökkentése a szabihosszal az adatbázisban
-            String sz = "UPDATE dolgozok SET maradek_szabadsag = maradek_szabadsag - ? WHERE nev = ?";
-            try (Connection kapcs = DriverManager.getConnection(dbUrl, user, pass);
-                    PreparedStatement ekp = kapcs.prepareStatement(sz)) {
-                ekp.setInt(1, szabadsagHossz(szabistart, szabivege));
-                ekp.setString(2, nev);
-                ekp.executeUpdate();
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-            //Szabadság bevitele a szabadságok táblába
-            String s = "INSERT INTO szabadsagok (dolgozoid,szabadsag_kezdete,szabadsag_vege,szabadsag_hossza) VALUES((SELECT id FROM dolgozok WHERE nev = ?),?,?,?);";
-            try (Connection kapcs = DriverManager.getConnection(dbUrl, user, pass);
-                    PreparedStatement ekp = kapcs.prepareStatement(s)) {
-                ekp.setString(1, nev);
-                ekp.setString(2, szabistart);
-                ekp.setString(3, szabivege);
-                ekp.setInt(4, szabadsagHossz(szabistart, szabivege));
-                ekp.executeUpdate();
-                
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-       }
-        
-    public void szabadsag_torles (int id){ 
+
+    public void uj_szabadsag(String nev, String szabistart, String szabivege) {
+
+        //maradekszabi csökkentése a szabihosszal az adatbázisban
+        String sz = "UPDATE dolgozok SET maradek_szabadsag = maradek_szabadsag - ? WHERE nev = ?";
+        try (Connection kapcs = DriverManager.getConnection(dbUrl, user, pass);
+                PreparedStatement ekp = kapcs.prepareStatement(sz)) {
+            ekp.setInt(1, szabadsagHossz(szabistart, szabivege));
+            ekp.setString(2, nev);
+            ekp.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        //Szabadság bevitele a szabadságok táblába
+        String s = "INSERT INTO szabadsagok (dolgozoid,szabadsag_kezdete,szabadsag_vege,szabadsag_hossza) VALUES((SELECT id FROM dolgozok WHERE nev = ?),?,?,?);";
+        try (Connection kapcs = DriverManager.getConnection(dbUrl, user, pass);
+                PreparedStatement ekp = kapcs.prepareStatement(s)) {
+            ekp.setString(1, nev);
+            ekp.setString(2, szabistart);
+            ekp.setString(3, szabivege);
+            ekp.setInt(4, szabadsagHossz(szabistart, szabivege));
+            ekp.executeUpdate();
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void szabadsag_torles(int id) {
         //szabi törlése
         String s = "DELETE FROM szabadsagok WHERE id = ?;";
         try (Connection kapcs = DriverManager.getConnection(dbUrl, user, pass);
-            PreparedStatement ekp = kapcs.prepareStatement(s)) {
+                PreparedStatement ekp = kapcs.prepareStatement(s)) {
             ekp.setInt(1, id);
             ekp.executeUpdate();
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
     }
-    
-        public void szabadsag_modositas(int id,String nev, String szabikezdete, String szabivege){
-        
-        
+
+    public void szabadsag_modositas(int id, String nev, String szabikezdete, String szabivege) {
+
         //A szabadság eredeti dátumainak lekérdezése
-        String regiSzabiKezdete = ""; 
-        String regiSzabiVege = ""; 
-        String sz = "SELECT szabadsag_kezdete,szabadsag_vege FROM szabadsagok WHERE id = ?;";    
+        String regiSzabiKezdete = "";
+        String regiSzabiVege = "";
+        String sz = "SELECT szabadsag_kezdete,szabadsag_vege FROM szabadsagok WHERE id = ?;";
         try (Connection kapcs = DriverManager.getConnection(dbUrl, user, pass);
                 PreparedStatement ekp = kapcs.prepareStatement(sz)) {
             ekp.setInt(1, id);
@@ -218,16 +218,14 @@ public class DB {
             regiSzabiVege = eredmeny.getString(2);
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
-        }    
-        
+        }
+
         if (!(szabikezdete.equals(regiSzabiKezdete) && szabivege.equals(regiSzabiVege))) {
-                    int regiSzabiHossz = szabadsagHossz(regiSzabiKezdete, regiSzabiVege);
-                    int szabiKulonbseg = regiSzabiHossz - szabadsagHossz(szabikezdete, szabivege);
-                    maradek_szabadsag_modositas(nev, szabiKulonbseg);
-                }
-        
-        
-        
+            int regiSzabiHossz = szabadsagHossz(regiSzabiKezdete, regiSzabiVege);
+            int szabiKulonbseg = regiSzabiHossz - szabadsagHossz(szabikezdete, szabivege);
+            maradek_szabadsag_modositas(nev, szabiKulonbseg);
+        }
+
         //szabadságok modosítása
         String s = "UPDATE szabadsagok SET dolgozoid = (SELECT id FROM dolgozok WHERE nev = ?),szabadsag_kezdete=?,szabadsag_vege=?,szabadsag_hossza = ? WHERE id =?;";
         try (Connection kapcs = DriverManager.getConnection(dbUrl, user, pass);
@@ -241,20 +239,19 @@ public class DB {
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
-        }
+    }
+
     /**
      *
      * @param szabistart
      * @param szabivege
      * @return
      */
-    private int szabadsagHossz (String szabistart,String szabivege) {
-            
-            LocalDate szabiStart = LocalDate.parse(szabistart, DateTimeFormatter.ISO_DATE);
-            LocalDate szabiVege = LocalDate.parse(szabivege, DateTimeFormatter.ISO_DATE);
-            int szabihossza = 0;
-        //a szabi kezdete korábbi dátum e mint a vége
-        if (!szabiVege.isBefore(szabiStart)) {
+    private int szabadsagHossz(String szabistart, String szabivege) {
+
+        LocalDate szabiStart = LocalDate.parse(szabistart, DateTimeFormatter.ISO_DATE);
+        LocalDate szabiVege = LocalDate.parse(szabivege, DateTimeFormatter.ISO_DATE);
+        int szabihossza = 0;
             //a dátumok hanyadik napok az évben
             int kezdonap = szabiStart.getDayOfYear();
             int vegenap = szabiVege.getDayOfYear();
@@ -277,28 +274,27 @@ public class DB {
                 szabihossza -= hetveginapok;
             }
             //extranapok vizsgálata
-            
+
             int extranap = 0;
             String zs = "SELECT ertek FROM extranapok WHERE datum BETWEEN ? AND ?";
             try (Connection kapcs = DriverManager.getConnection(dbUrl, user, pass);
-                PreparedStatement ekp = kapcs.prepareStatement(zs)) {
+                    PreparedStatement ekp = kapcs.prepareStatement(zs)) {
                 ekp.setString(1, szabistart);
                 ekp.setString(2, szabivege);
                 ResultSet eredmeny = ekp.executeQuery();
-                if (eredmeny.next())
-                    if (!eredmeny.wasNull()){
+                if (eredmeny.next()) {
+                    if (!eredmeny.wasNull()) {
                         extranap += eredmeny.getInt(1);
                         szabihossza += extranap;
-                    } 
-                    
+                    }
+                }
+
             } catch (SQLException ex) {
                 System.out.println(ex.getMessage());
             }
-            
-            
+
         
-        } 
-            return szabihossza;
-        } 
-        
+        return szabihossza;
+    }
+
 }
--- src/szabadsag_nyilvantartas/FXMLDocumentController.java
+++ src/szabadsag_nyilvantartas/FXMLDocumentController.java
@@ -11,6 +11,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
@@ -23,8 +24,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
  * @author Polgár Béla
  */
 public class FXMLDocumentController implements Initializable {
-    
-     @FXML
+
+    @FXML
     private TextField txtNev;
 
     @FXML
@@ -84,7 +85,7 @@ public class FXMLDocumentController implements Initializable {
         if (i > -1) {
             int id = tblDolgozok.getItems().get(i).getId();
             dolgozoDB.dolgozo_modositas(id, txtNev.getText(), dpSzulDatum.getValue().toString(), cbGyerekDB.getValue());
-            dolgozoDB.dolgozo_lista(tblDolgozok.getItems(),cbNev.getItems());
+            dolgozoDB.dolgozo_lista(tblDolgozok.getItems(), cbNev.getItems());
             tblDolgozok.getSelectionModel().select(i);
         }
     }
@@ -92,12 +93,20 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     void modositSzabadsag() {
         int i = tblSzabadsagok.getSelectionModel().getFocusedIndex();
-        if (i > -1){
+        if (i > -1) {
             int id = tblSzabadsagok.getItems().get(i).getId();
-            szabadsagDB.szabadsag_modositas(id, cbNev.getValue(), dpSzabiKezdet.getValue().toString(), dpSzabiVege.getValue().toString());
-            szabadsagDB.szabadsag_lista(tblSzabadsagok.getItems());
-            dolgozoDB.dolgozo_lista(tblDolgozok.getItems(), cbNev.getItems());
-            tblSzabadsagok.getSelectionModel().select(i);
+            if (dpSzabiVege.getValue().isBefore(dpSzabiKezdet.getValue())) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Hibás Dátum");
+                alert.setHeaderText(null);
+                alert.setContentText("A szabadság vége nem lehet korábbi dátum");
+                alert.showAndWait();
+            } else {
+                szabadsagDB.szabadsag_modositas(id, cbNev.getValue(), dpSzabiKezdet.getValue().toString(), dpSzabiVege.getValue().toString());
+                szabadsagDB.szabadsag_lista(tblSzabadsagok.getItems());
+                dolgozoDB.dolgozo_lista(tblDolgozok.getItems(), cbNev.getItems());
+                tblSzabadsagok.getSelectionModel().select(i);
+            }
         }
     }
 
@@ -107,15 +116,16 @@ public class FXMLDocumentController implements Initializable {
         if (i > -1) {
             int id = tblDolgozok.getItems().get(i).getId();
             dolgozoDB.dolgozo_torles(id);
-            dolgozoDB.dolgozo_lista(tblDolgozok.getItems(),cbNev.getItems());
+            dolgozoDB.dolgozo_lista(tblDolgozok.getItems(), cbNev.getItems());
             szabadsagDB.szabadsag_lista(tblSzabadsagok.getItems());
-            int utolso = tblDolgozok.getItems().size()-1;
-            if (i <= utolso)
+            int utolso = tblDolgozok.getItems().size() - 1;
+            if (i <= utolso) {
                 tblDolgozok.getSelectionModel().select(i);
-            else if (i > 0)
-                tblDolgozok.getSelectionModel().select(i-1);
+            } else if (i > 0) {
+                tblDolgozok.getSelectionModel().select(i - 1);
+            }
         }
-    
+
     }
 
     @FXML
@@ -125,18 +135,19 @@ public class FXMLDocumentController implements Initializable {
             int id = tblSzabadsagok.getItems().get(i).getId();
             int szabiHossza = tblSzabadsagok.getItems().get(i).getSzabiHossza();
             String nev = tblSzabadsagok.getItems().get(i).getNev();
-            szabadsagDB.maradek_szabadsag_modositas(nev,szabiHossza);
+            szabadsagDB.maradek_szabadsag_modositas(nev, szabiHossza);
             szabadsagDB.szabadsag_torles(id);
             szabadsagDB.szabadsag_lista(tblSzabadsagok.getItems());
             dolgozoDB.dolgozo_lista(tblDolgozok.getItems(), cbNev.getItems());
-            int utolso = tblSzabadsagok.getItems().size()-1;
-            if (i <= utolso)
+            int utolso = tblSzabadsagok.getItems().size() - 1;
+            if (i <= utolso) {
                 tblSzabadsagok.getSelectionModel().select(i);
-            else
-                tblSzabadsagok.getSelectionModel().select(i-1);
-            
+            } else {
+                tblSzabadsagok.getSelectionModel().select(i - 1);
+            }
+
         }
-        
+
     }
 
     @FXML
@@ -147,62 +158,72 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     void ujSzabadsag() {
-        szabadsagDB.uj_szabadsag(cbNev.getValue(), dpSzabiKezdet.getValue().toString(), dpSzabiVege.getValue().toString());
-        szabadsagDB.szabadsag_lista(tblSzabadsagok.getItems());
-        dolgozoDB.dolgozo_lista(tblDolgozok.getItems(), cbNev.getItems());
+        if (dpSzabiVege.getValue().isBefore(dpSzabiKezdet.getValue())) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Hibás Dátum");
+            alert.setHeaderText(null);
+            alert.setContentText("A szabadság vége nem lehet korábbi dátum");
+            alert.showAndWait();
+        } else {
+            szabadsagDB.uj_szabadsag(cbNev.getValue(), dpSzabiKezdet.getValue().toString(), dpSzabiVege.getValue().toString());
+            szabadsagDB.szabadsag_lista(tblSzabadsagok.getItems());
+            dolgozoDB.dolgozo_lista(tblDolgozok.getItems(), cbNev.getItems());
+        }
     }
-    
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         //dolgozók tábla
-        cbGyerekDB.getItems().addAll(0,1,2,3,4,5,6,7,8,9);
-        
+        cbGyerekDB.getItems().addAll(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
         oNev.setCellValueFactory(new PropertyValueFactory<>("nev"));
         oSzulDatum.setCellValueFactory(new PropertyValueFactory<>("szuletesiDatum"));
         oGyerekDB.setCellValueFactory(new PropertyValueFactory<>("gyerekDB"));
         oEvesSzabi.setCellValueFactory(new PropertyValueFactory<>("evesSzabadsag"));
         oMaradekSzabi.setCellValueFactory(new PropertyValueFactory<>("maradekSzabadsag"));
         oFizuNelkul.setCellValueFactory(new PropertyValueFactory<>("fizetesNelkuli"));
-        
+
         dpSzulDatum.setValue(LocalDate.parse("1980-01-01"));
         cbGyerekDB.setValue(0);
-        
+
         dolgozoDB.dolgozo_lista(tblDolgozok.getItems(), cbNev.getItems());
-        
-        tblDolgozok.getSelectionModel().selectedIndexProperty().addListener((o,regi,uj) -> dolgozok_tablabol(uj.intValue()));
+
+        tblDolgozok.getSelectionModel().selectedIndexProperty().addListener((o, regi, uj) -> dolgozok_tablabol(uj.intValue()));
         //szabadság tábla
         oNevSz.setCellValueFactory(new PropertyValueFactory<>("nev"));
         oSzabiKezdet.setCellValueFactory(new PropertyValueFactory<>("szabiKezdete"));
         oSzabiVege.setCellValueFactory(new PropertyValueFactory<>("szabiVege"));
         oSzabiHossza.setCellValueFactory(new PropertyValueFactory<>("szabiHossza"));
-        
+
         dpSzabiKezdet.setValue(LocalDate.now());
         dpSzabiVege.setValue(dpSzabiKezdet.getValue());
-        
+
         szabadsagDB.szabadsag_lista(tblSzabadsagok.getItems());
-        
-        tblSzabadsagok.getSelectionModel().selectedIndexProperty().addListener((o,regi,uj) -> szabadsagok_tablabol(uj.intValue()));
-    }  
-    
-       
+
+        tblSzabadsagok.getSelectionModel().selectedIndexProperty().addListener((o, regi, uj) -> szabadsagok_tablabol(uj.intValue()));
+    }
+
     DB dolgozoDB = new DB();
     DB szabadsagDB = new DB();
-    
+
     private void dolgozok_tablabol(int i) {
-        if (i == -1) return;
+        if (i == -1) {
+            return;
+        }
         Dolgozo d = tblDolgozok.getItems().get(i);
         txtNev.setText(d.getNev());
         dpSzulDatum.setValue(LocalDate.parse(d.getSzuletesiDatum()));
         cbGyerekDB.setValue(d.getGyerekDB());
     }
-    
+
     private void szabadsagok_tablabol(int i) {
-        if (i == -1) return;
+        if (i == -1) {
+            return;
+        }
         Szabadsag sz = tblSzabadsagok.getItems().get(i);
         cbNev.setValue(sz.getNev());
         dpSzabiKezdet.setValue(LocalDate.parse(sz.getSzabiKezdete()));
         dpSzabiVege.setValue(LocalDate.parse(sz.getSzabiVege()));
     }
-    
+
 }
