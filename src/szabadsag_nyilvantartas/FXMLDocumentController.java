dd298ed90f3103014ca479e4d40d541780263d12
Polgár Béla <S@Lenovo-PC>
2019-03-03 12:09:29 +0100

    Adatok táblába töltése

--- src/szabadsag_nyilvantartas/FXMLDocumentController.java
+++ src/szabadsag_nyilvantartas/FXMLDocumentController.java
@@ -6,6 +6,7 @@
 package szabadsag_nyilvantartas;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,6 +16,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  *
@@ -29,52 +31,52 @@ public class FXMLDocumentController implements Initializable {
     private DatePicker dpSzulDatum;
 
     @FXML
-    private ComboBox<?> cbGyerekDB;
+    private ComboBox<Integer> cbGyerekDB;
 
     @FXML
-    private TableView<?> tblDolgozok;
+    private TableView<Dolgozo> tblDolgozok;
 
     @FXML
-    private TableColumn<?, ?> oNev;
+    private TableColumn<Dolgozo, String> oNev;
 
     @FXML
-    private TableColumn<?, ?> oSzulDatum;
+    private TableColumn<Dolgozo, String> oSzulDatum;
 
     @FXML
-    private TableColumn<?, ?> oGyerekDB;
+    private TableColumn<Dolgozo, Integer> oGyerekDB;
 
     @FXML
-    private TableColumn<?, ?> oEvesSzabi;
+    private TableColumn<Dolgozo, Integer> oEvesSzabi;
 
     @FXML
-    private TableColumn<?, ?> oMaradekSzabi;
+    private TableColumn<Dolgozo, Integer> oMaradekSzabi;
 
     @FXML
-    private TableColumn<?, ?> oFizuNelkul;
+    private TableColumn<Dolgozo, Integer> oFizuNelkul;
 
     @FXML
     private DatePicker dpSzabiKezdet;
 
     @FXML
-    private DatePicker dbSzabiVege;
+    private DatePicker dpSzabiVege;
 
     @FXML
-    private ComboBox<?> cbNev;
+    private ComboBox<String> cbNev;
 
     @FXML
-    private TableView<?> tblSzabadsagok;
+    private TableView<Szabadsag> tblSzabadsagok;
 
     @FXML
-    private TableColumn<?, ?> oNevSz;
+    private TableColumn<Szabadsag, String> oNevSz;
 
     @FXML
-    private TableColumn<?, ?> oSzabiKezdet;
+    private TableColumn<Szabadsag, String> oSzabiKezdet;
 
     @FXML
-    private TableColumn<?, ?> oSzabiVege;
+    private TableColumn<Szabadsag, String> oSzabiVege;
 
     @FXML
-    private TableColumn<?, ?> oSzabiHossza;
+    private TableColumn<Szabadsag, Integer> oSzabiHossza;
 
     @FXML
     void modositDolgozo() {
@@ -109,7 +111,46 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        //dolgozók tábla        
+        oNev.setCellValueFactory(new PropertyValueFactory<>("nev"));
+        oSzulDatum.setCellValueFactory(new PropertyValueFactory<>("szuletesiDatum"));
+        oGyerekDB.setCellValueFactory(new PropertyValueFactory<>("gyerekDB"));
+        oEvesSzabi.setCellValueFactory(new PropertyValueFactory<>("evesSzabadsag"));
+        oMaradekSzabi.setCellValueFactory(new PropertyValueFactory<>("maradekSzabadsag"));
+        oFizuNelkul.setCellValueFactory(new PropertyValueFactory<>("fizetesNelkuli"));
+        
+        dolgozoDB.dolgozo_lista(tblDolgozok.getItems(), cbNev.getItems());
+        
+        tblDolgozok.getSelectionModel().selectedIndexProperty().addListener((o,regi,uj) -> dolgozok_tablabol(uj.intValue()));
+        //szabadság tábla
+        oNevSz.setCellValueFactory(new PropertyValueFactory<>("nev"));
+        oSzabiKezdet.setCellValueFactory(new PropertyValueFactory<>("szabiKezdete"));
+        oSzabiVege.setCellValueFactory(new PropertyValueFactory<>("szabiVege"));
+        oSzabiHossza.setCellValueFactory(new PropertyValueFactory<>("szabiHossza"));
+        
+        szabadsagDB.szabadsag_lista(tblSzabadsagok.getItems());
+        
+        tblSzabadsagok.getSelectionModel().selectedIndexProperty().addListener((o,regi,uj) -> szabadsagok_tablabol(uj.intValue()));
+    }  
+    
+       
+    DB dolgozoDB = new DB();
+    DB szabadsagDB = new DB();
+    
+    private void dolgozok_tablabol(int i) {
+        if (i == -1) return;
+        Dolgozo d = tblDolgozok.getItems().get(i);
+        txtNev.setText(d.getNev());
+        dpSzulDatum.setValue(LocalDate.parse(d.getSzuletesiDatum()));
+        cbGyerekDB.setValue(d.getGyerekDB());
+    }
+    
+    private void szabadsagok_tablabol(int i) {
+        if (i == -1) return;
+        Szabadsag sz = tblSzabadsagok.getItems().get(i);
+        cbNev.setValue(sz.getNev());
+        dpSzabiKezdet.setValue(LocalDate.parse(sz.getSzabiKezdete()));
+        dpSzabiVege.setValue(LocalDate.parse(sz.getSzabiVege()));
+    }
     
 }
